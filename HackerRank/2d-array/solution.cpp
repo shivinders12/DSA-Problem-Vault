61c99d3646dbd23046e8d5d6459b2943234b39b8
shivinders12 <singhshivinder.12@gmail.com>
2026-08-16 14:28:20 +0530

    Sync HackerRank: 2D Array - DS

---
+++ HackerRank/2d-array/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int hourglassSum(vector<vector<int>> arr) {
+    int maximum = INT_MIN;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+
+            int sum = arr[i][j] 
+                    + arr[i][j+1] 
+                    + arr[i][j+2]
+                    + arr[i+1][j+1]
+                    + arr[i+2][j] 
+                    + arr[i+2][j+1] 
+                    + arr[i+2][j+2];
+
+            maximum = max(maximum, sum);
