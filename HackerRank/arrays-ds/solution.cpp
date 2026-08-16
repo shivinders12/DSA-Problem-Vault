41a40d373a6066feb787ca85f56a25d56fd7381f
shivinders12 <singhshivinder.12@gmail.com>
2026-08-16 14:27:24 +0530

    Sync HackerRank: Arrays - DS

---
+++ HackerRank/arrays-ds/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+vector<int> reverseArray(vector<int> a) {
+    reverse(a.begin(), a.end());
+    return a ;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string arr_count_temp;
+    getline(cin, arr_count_temp);
+
+    int arr_count = stoi(ltrim(rtrim(arr_count_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
